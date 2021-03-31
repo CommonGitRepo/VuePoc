42907eac12d1c1f0a13be4940dea34f8e09a6b8a
CommonGitRepo <commongitrepo@gmail.com>
2021-03-31 08:24:49 +0530

    UI Changes

--- hospitaltracker/src/main.ts
+++ hospitaltracker/src/main.ts
@@ -3,6 +3,18 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import "./plugins/bootstrap-vue";
+import { ValidationObserver, ValidationProvider, extend } from 'vee-validate';
+import * as rules from 'vee-validate/dist/rules';
+
+
+Object.keys(rules)
+.forEach(rule=>{
+    extend(rule,rules[rule])
+})
+
+Vue.component('ValidationObserver', ValidationObserver);
+Vue.component('ValidationProvider', ValidationProvider);
+
 Vue.config.productionTip = false;
 
 new Vue({
